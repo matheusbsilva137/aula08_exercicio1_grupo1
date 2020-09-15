a859b34c0a93ab63e79cd3d901cbf3954125664e
artchier <ar-chaves@hotmail.com>
2020-09-14 21:51:32 -0300

    Arthur

---
+++ aula08_exercicio1/src/br/digital/com/Curso.kt
@@ -0,0 +1,11 @@
+package br.digital.com
+
+class Curso (var nome: String, var professorResponsavel: Professor{
+
+    val listaDeAlunos: MutableList<Aluno> = ArrayList()
+    val listaDeAulas: MutableList<Aula> = ArrayList()
+
+    fun adicionarAluno(vararg aluno: Aluno) = listaDeAlunos.addAll(aluno)
+
+    fun adicionarAula(vararg aula: Aula) = listaDeAulas.addAll(aula)
+}
